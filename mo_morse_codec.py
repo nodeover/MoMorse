96aebf6321759e2481986680d64cd3e0888a74a3
YM Yang <airtag@outlook.com>
2024-03-29 09:47:57 +0900

    Update and rename morse_decoder.py to mo_morse_codec.py

---
+++ mo_morse_codec.py
@@ -0,0 +1,69 @@
+morse_to_alpha = {
+    '.-': 'A', '-...': 'B', '-.-.': 'C', '-..': 'D', '.': 'E', '..-.': 'F',
+    '--.': 'G', '....': 'H', '..': 'I', '.---': 'J', '-.-': 'K', '.-..': 'L',
+    '--': 'M', '-.': 'N', '---': 'O', '.--.': 'P', '--.-': 'Q', '.-.': 'R',
+    '...': 'S', '-': 'T', '..-': 'U', '...-': 'V', '.--': 'W', '-..-': 'X',
+    '-.--': 'Y', '--..': 'Z', '..--..': '?', '-..-.': '/', '--..--': ',', 
+    '.----': '1', '..---': '2', '...--': '3', '....-': '4', '.....': '5',
+    '-....': '6', '--...': '7', '---..': '8', '----.': '9', '-----': '0',
+    '.-.-.-': '.', '--..--': ',', '..--..': '?', '-.-.--': '!', '-....-': '-',
+    '-..-.': '/', '.--.-.': '@', '.-..-.': '"', '..--.-': '_', '---...': ':',
+    '.----.': "'", '.-.-.': '+', '-.--.': '(', '-.--.-': ')', '.-...': '&',
+    '---..-': '=', '..-.-': ' ', '..--../': ' '
+}
+def mo_morse_string_decoder(morse_code):
+    decoded_message = ''
+    for word in morse_code.split('/'):
+        for letter in word.split():
+            decoded_message += morse_to_alpha.get(letter, '')
+        decoded_message += ' '
+    decoded_message.strip()
+    return decoded_message
+    
+morse_to_alpha = {
+    '.-': 'A', '-...': 'B', '-.-.': 'C', '-..': 'D', '.': 'E', '..-.': 'F',
+    '--.': 'G', '....': 'H', '..': 'I', '.---': 'J', '-.-': 'K', '.-..': 'L',
+    '--': 'M', '-.': 'N', '---': 'O', '.--.': 'P', '--.-': 'Q', '.-.': 'R',
+    '...': 'S', '-': 'T', '..-': 'U', '...-': 'V', '.--': 'W', '-..-': 'X',
+    '-.--': 'Y', '--..': 'Z', '..--..': '?', '-..-.': '/', '--..--': ',', 
+    '.----': '1', '..---': '2', '...--': '3', '....-': '4', '.....': '5',
+    '-....': '6', '--...': '7', '---..': '8', '----.': '9', '-----': '0',
+    '.-.-.-': '.', '--..--': ',', '..--..': '?', '-.-.--': '!', '-....-': '-',
+    '-..-.': '/', '.--.-.': '@', '.-..-.': '"', '..--.-': '_', '---...': ':',
+    '.----.': "'", '.-.-.': '+', '-.--.': '(', '-.--.-': ')', '.-...': '&',
+    '---..-': '=', '..-.-': ' ', '..--../': ' '
+}
+alpha_to_morse = {value: key for key, value in morse_to_alpha.items()}
+
+
+def mo_morse_string_decoder(morse_code):
+    decoded_message = ''
+    for word in morse_code.split('/'):
+        for letter in word.split():
+            decoded_message += morse_to_alpha.get(letter, '')
+        decoded_message += ' '
+    decoded_message.strip()
+    return decoded_message
+    
+
+def mo_morse_string_encoder(text):
+    text = text.upper().strip()
+    encoded_message = ''
+    
+    for word in text.split(' '):
+        for letter in word:
+            # Look up the Morse code for each letter and add a space to separate Morse characters within a word
+            encoded_message += alpha_to_morse.get(letter, '') + ' '
+        # Add a slash to separate words in Morse code
+        encoded_message = encoded_message.rstrip() + '/'
+        
+    # Remove the trailing slash and return the encoded message
+    return encoded_message.rstrip('/')
+
+
+test_string = 'HELLO? NICE TO MEET YOU'
+encoded_test_string = mo_morse_string_encoder(test_string)
+decoded_test_string = mo_morse_string_decoder(encoded_test_string)
+print(encoded_test_string)
+print(decoded_test_string)
+
